7c81a316aaa8e6cb5b0399aa2ecb1f393939bf0f
Severin Dellsperger <severin.dellsperger@gmail.com>
2024-02-14 16:34:52 +0100

    refactor arango output

--- pkg/output/arango.go
+++ pkg/output/arango.go
@@ -76,129 +76,97 @@ func (output *ArangoOutput) getDatabase() (error, driver.Database) {
 	return nil, db
 }
 
-// func (output *ArangoOutput) getAQLQuery(command message.ArangoUpdateCommand) string {
-// 	var filters []string
-// 	for key, val := range command.FilterBy {
-// 		filters = append(filters, fmt.Sprintf("d.%s == '%s'", key, val))
-// 	}
-// 	filterStr := strings.Join(filters, " && ")
+func (output *ArangoOutput) updateField(field *uint32, value json.Number) {
+	if intValue, err := value.Int64(); err == nil {
+		*field = uint32(intValue)
+	} else if floatValue, err := value.Float64(); err == nil {
+		*field = uint32(floatValue)
+	} else {
+		output.log.Errorf("Failed to convert json.Number to int64 or float64")
+	}
+}
 
-// 	var updateStr string
-// 	if command.Index != nil {
-// 		updateStr = fmt.Sprintf("LET updatedArray = (FOR i IN RANGE(0, LENGTH(d.%s) - 1) RETURN i == %d ? %v : d.%s[i]) UPDATE d WITH { %s: updatedArray } IN %s", command.Field, *command.Index, command.Value, command.Field, command.Field, command.Collection)
-// 	} else {
-// 		updateStr = fmt.Sprintf("UPDATE d WITH { %s: %v } IN %s", command.Field, command.Value, command.Collection)
-// 	}
-
-// 	query := fmt.Sprintf("FOR d IN %s FILTER %s %s", command.Collection, filterStr, updateStr)
-// 	output.log.Debugf("Generated AQL query: %s", query)
-// 	return query
-// }
-
-// func (output *ArangoOutput) executeQuery(db driver.Database, query string) error {
-// 	ctx := driver.WithWaitForSync(context.Background())
-// 	_, err := db.Query(ctx, query, nil)
-// 	if err != nil {
-// 		output.log.Errorf("Error executing query: %v", err)
-// 	}
-// 	return err
-// }
-
-// func (output *ArangoOutput) processArangoUpdateCommand(command message.ArangoUpdateCommand) {
-// 	output.log.Debugf("Processing Arango update for collection: %s", command.Collection)
-// 	err, db := output.getDatabase()
-// 	if err != nil {
-// 		output.log.Errorf("Error getting database: %v", err)
-// 		return
-// 	}
-
-// 	query := output.getAQLQuery(command)
-
-// 	if err := output.executeQuery(db, query); err != nil {
-// 		output.log.Errorf("Error executing query: %v", err)
-// 	}
-// }
-
-func (output *ArangoOutput) updateLsLink(ctx context.Context, cursor driver.Cursor, command message.ArangoUpdateCommand) (error, []string, []arango.LSLink) {
-	keys := make([]string, 0)
-	lsLinks := make([]arango.LSLink, 0)
-	counter := 0
-	for {
-		lsLink := arango.LSLink{}
-		_, err := cursor.ReadDocument(ctx, &lsLink)
-		if driver.IsNoMoreDocuments(err) {
-			break
-		} else if err != nil {
-			output.log.Errorf("Error reading document: %v", err)
-			return err, nil, nil
-		}
-		arangoUpdate, ok := command.Updates[lsLink.LocalLinkIP]
-		if !ok {
-			output.log.Errorf("No update found for local link IP: %s", lsLink.LocalLinkIP)
-			continue
-		}
+func (output *ArangoOutput) processLsLinkDocument(ctx context.Context, cursor driver.Cursor, command message.ArangoUpdateCommand, lsLinks []arango.LSLink, keys []string, i int) (int, error) {
+	lsLink := arango.LSLink{}
+	_, err := cursor.ReadDocument(ctx, &lsLink)
+	if err != nil {
+		return i, err
+	}
+	arangoUpdate, ok := command.Updates[lsLink.LocalLinkIP]
+	if !ok {
+		output.log.Errorf("No update found for local link IP: %s", lsLink.LocalLinkIP)
+		return i, nil
+	}
+	if jsonNumber, ok := arangoUpdate.Value.(json.Number); ok {
 		if arangoUpdate.Field == "unidir_link_delay" {
-			if jsonNumber, ok := arangoUpdate.Value.(json.Number); ok {
-				if intValue, err := jsonNumber.Int64(); err == nil {
-					lsLink.UnidirLinkDelay = uint32(intValue)
-				} else if floatValue, err := jsonNumber.Float64(); err == nil {
-					lsLink.UnidirLinkDelay = uint32(floatValue)
-				} else {
-					output.log.Errorf("Failed to convert json.Number to int64 or float64")
-				}
-			} else {
-				output.log.Errorf("Failed to convert interface{} to json.Number")
-			}
+			output.updateField(&lsLink.UnidirLinkDelay, jsonNumber)
 		} else if arangoUpdate.Field == "unidir_link_delay_min_max" {
 			if arangoUpdate.Index == nil || *arangoUpdate.Index > 1 {
 				output.log.Errorf("Unknown index: %v", arangoUpdate.Index)
-				continue
-			} else if *arangoUpdate.Index == 0 {
-				if jsonNumber, ok := arangoUpdate.Value.(json.Number); ok {
-					if intValue, err := jsonNumber.Int64(); err == nil {
-						lsLink.UnidirLinkDelayMinMax[0] = uint32(intValue)
-					} else if floatValue, err := jsonNumber.Float64(); err == nil {
-						lsLink.UnidirLinkDelayMinMax[0] = uint32(floatValue)
-					} else {
-						output.log.Errorf("Failed to convert json.Number to int64 or float64")
-					}
-				} else {
-					output.log.Errorf("Failed to convert interface{} to json.Number")
-				}
-			} else if *arangoUpdate.Index == 1 {
-				if jsonNumber, ok := arangoUpdate.Value.(json.Number); ok {
-					if intValue, err := jsonNumber.Int64(); err == nil {
-						lsLink.UnidirLinkDelayMinMax[1] = uint32(intValue)
-					} else if floatValue, err := jsonNumber.Float64(); err == nil {
-						lsLink.UnidirLinkDelayMinMax[1] = uint32(floatValue)
-					} else {
-						output.log.Errorf("Failed to convert json.Number to int64 or float64")
-					}
-				} else {
-					output.log.Errorf("Failed to convert interface{} to json.Number")
-				}
+				return i, nil
 			}
+			output.updateField(&lsLink.UnidirLinkDelayMinMax[*arangoUpdate.Index], jsonNumber)
 		}
-		lsLinks = append(lsLinks, lsLink)
-		keys = append(keys, lsLink.Key)
-		counter++
+	} else {
+		output.log.Errorf("Failed to convert interface{} to json.Number")
 	}
-	return nil, keys, lsLinks
+	lsLinks[i] = lsLink
+	keys[i] = lsLink.Key
+	return i + 1, nil
+}
+
+func (output *ArangoOutput) updateLsLink(ctx context.Context, cursor driver.Cursor, command message.ArangoUpdateCommand) (error, []string, []arango.LSLink) {
+	count := cursor.Count()
+	keys := make([]string, count)
+	lsLinks := make([]arango.LSLink, count)
+	i := 0
+	var err error
+	for {
+		i, err = output.processLsLinkDocument(ctx, cursor, command, lsLinks, keys, i)
+		if driver.IsNoMoreDocuments(err) {
+			return nil, keys[:i], lsLinks[:i]
+		} else if err != nil {
+			output.log.Errorf("Unexpected error processing document: %v", err)
+			continue
+		}
+	}
+}
+
+func (output *ArangoOutput) executeQuery(ctx context.Context, db driver.Database, command message.ArangoUpdateCommand) (driver.Cursor, error) {
+	query := fmt.Sprintf("FOR d IN %s RETURN d", command.Collection)
+	ctx = driver.WithQueryCount(ctx, true)
+	cursor, err := db.Query(ctx, query, nil)
+	if err != nil {
+		output.log.Errorf("Error executing query: %v", err)
+		return nil, err
+	}
+	return cursor, err
+}
+
+func (output *ArangoOutput) updateDocuments(ctx context.Context, db driver.Database, command message.ArangoUpdateCommand, keys []string, lsLinks []arango.LSLink) error {
+	col, err := db.Collection(ctx, command.Collection)
+	if err != nil {
+		output.log.Errorf("Error getting collection: %v", err)
+		return err
+	}
+	_, errSlice, err := col.UpdateDocuments(ctx, keys, lsLinks)
+	if err != nil {
+		output.log.Errorf("Error updating documents: %v %v", errSlice, err)
+		return err
+	}
+	return nil
 }
 
 func (output *ArangoOutput) processArangoUpdateCommand(command message.ArangoUpdateCommand) {
 	output.log.Infof("Processing Arango update for collection: %s", command.Collection)
-	// output.log.Debugf("Processing Arango update for collection: %s", command.Collection)
 	err, db := output.getDatabase()
 	if err != nil {
 		output.log.Errorf("Error getting database: %v", err)
 		return
 	}
 	ctx := context.Background()
-	query := fmt.Sprintf("FOR d IN %s RETURN d", command.Collection)
-	cursor, err := db.Query(ctx, query, nil)
+	cursor, err := output.executeQuery(ctx, db, command)
 	if err != nil {
-		output.log.Errorf("Error executing query: %v", err)
 		return
 	}
 	defer cursor.Close()
@@ -209,14 +177,9 @@ func (output *ArangoOutput) processArangoUpdateCommand(command message.ArangoUpd
 			output.log.Errorf("Error updating ls_link: %v", err)
 			return
 		}
-		col, err := db.Collection(ctx, command.Collection)
-		if err != nil {
-			output.log.Errorf("Error getting collection: %v", err)
-			return
-		}
-		_, errSlice, err := col.UpdateDocuments(ctx, keys, lsLinks)
-		if err != nil {
-			output.log.Errorf("Error updating documents: %v %v", errSlice, err)
+		output.log.Infof("Updating %d documents", len(keys))
+		if err := output.updateDocuments(ctx, db, command, keys, lsLinks); err != nil {
+			output.log.Errorf("Error updating documents: %v", err)
 			return
 		}
 	} else {
