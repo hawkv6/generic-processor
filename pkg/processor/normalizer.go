53bb668d4158b063545f945aed0fd601ebc7dded
severindellsperger <severin.dellsperger@gmail.com>
2024-07-29 11:46:47 +0200

    test normalization

--- pkg/processor/normalizer.go
+++ pkg/processor/normalizer.go
@@ -10,24 +10,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type StatisticalData struct {
-	q1                 float64
-	q3                 float64
-	interQuartileRange float64
-	lowerFence         float64
-	upperFence         float64
-}
-
-func NewStatisticalData(q1, q3, iqr, lowerFence, upperFence float64) *StatisticalData {
-	return &StatisticalData{
-		q1:                 q1,
-		q3:                 q3,
-		interQuartileRange: iqr,
-		lowerFence:         lowerFence,
-		upperFence:         upperFence,
-	}
-}
-
 type Normalizer interface {
 	NormalizeValues(map[string]message.ArangoUpdate, string, string) (*StatisticalData, error)
 	AddDataToNormalize(string, float64)
@@ -49,41 +31,36 @@ func NewMinMaxNormalizer() *MinMaxNormalizer {
 func (normalizer *MinMaxNormalizer) AddDataToNormalize(fieldName string, value float64) {
 	if _, ok := normalizer.normalizationData[fieldName]; !ok {
 		normalizer.normalizationData[fieldName] = make([]float64, 0)
-	} else {
-		normalizer.normalizationData[fieldName] = append(normalizer.normalizationData[fieldName], value)
 	}
+	normalizer.normalizationData[fieldName] = append(normalizer.normalizationData[fieldName], value)
 }
 
 func (normalizer *MinMaxNormalizer) ResetNormalizationData() {
 	normalizer.normalizationData = make(map[string][]float64)
 }
 
-func (normalizer *MinMaxNormalizer) getFences(data stats.Float64Data, upperFence, lowerFence *float64) (*StatisticalData, error) {
+func (normalizer *MinMaxNormalizer) calculateQuartiles(data stats.Float64Data) (*stats.Quartiles, float64, error) {
 	quartiles, err := stats.Quartile(data)
-	if err != nil {
-		return nil, fmt.Errorf("Error calculating quartiles: %v", err)
+	if err != nil || math.IsNaN(quartiles.Q1) || math.IsNaN(quartiles.Q2) || math.IsNaN(quartiles.Q3) {
+		return nil, 0, fmt.Errorf("Error calculating quartiles %v", err)
 	}
 	interQuartileRange := quartiles.Q3 - quartiles.Q1
 	normalizer.log.Debugln("Q1: ", quartiles.Q1)
 	normalizer.log.Debugln("Q2 / Median: ", quartiles.Q2)
 	normalizer.log.Debugln("Q3: ", quartiles.Q3)
 	normalizer.log.Debugln("Interquartile range: ", interQuartileRange)
-	outliers, err := stats.QuartileOutliers(data)
-	if err != nil {
-		return nil, fmt.Errorf("Error calculating outliers: %v", err)
-	}
+	outliers, _ := stats.QuartileOutliers(data)
 	normalizer.log.Debugf("Outliers: %+v", outliers)
+	return &quartiles, interQuartileRange, nil
+}
 
-	min, err := stats.Min(data)
+func (normalizer *MinMaxNormalizer) getFences(data stats.Float64Data, upperFence, lowerFence *float64) (*StatisticalData, error) {
+	quartiles, interQuartileRange, err := normalizer.calculateQuartiles(data)
 	if err != nil {
-		return nil, fmt.Errorf("Error calculating min: %v", err)
+		return nil, err
 	}
-
-	max, err := stats.Max(data)
-	if err != nil {
-		return nil, fmt.Errorf("Error calculating max: %v", err)
-	}
-
+	min, _ := stats.Min(data)
+	max, _ := stats.Max(data)
 	*upperFence = math.Min(quartiles.Q3+1.5*interQuartileRange, max)
 	normalizer.log.Debugln("Upper fence: ", *upperFence)
 	*lowerFence = math.Max(quartiles.Q1-1.5*interQuartileRange, min)
@@ -92,11 +69,11 @@ func (normalizer *MinMaxNormalizer) getFences(data stats.Float64Data, upperFence
 }
 
 func (normalizer *MinMaxNormalizer) getNormalizedValue(value float64, upperFence, lowerFence float64) float64 {
-	normalizedValue := value
+	normalizedValue := 1.0
 	if upperFence != lowerFence {
 		normalizedValue = (value - lowerFence) / (upperFence - lowerFence)
 		if normalizedValue <= 0 {
-			normalizedValue = 0.0000000001
+			normalizedValue = -math.MaxFloat64 // arango does not accept zero value
 		} else if normalizedValue > 1 {
 			normalizedValue = 1
 		}
@@ -104,6 +81,18 @@ func (normalizer *MinMaxNormalizer) getNormalizedValue(value float64, upperFence
 		normalizer.log.Warnf("Upper fence and lower fence are equal: %v", upperFence)
 	}
 	return normalizedValue
+}
+
+func (normalizer *MinMaxNormalizer) normalizeUpdates(updates map[string]message.ArangoUpdate, upperFence float64, lowerFence float64, normalizationField string) {
+	for key, update := range updates {
+		if len(update.Values) == 0 {
+			continue
+		}
+		normalizedValue := normalizer.getNormalizedValue(update.Values[len(update.Values)-1], upperFence, lowerFence)
+		update.Fields = append(update.Fields, normalizationField)
+		update.Values = append(update.Values, normalizedValue)
+		updates[key] = update
+	}
 }
 
 func (normalizer *MinMaxNormalizer) NormalizeValues(updates map[string]message.ArangoUpdate, inputField, normalizationField string) (*StatisticalData, error) {
@@ -114,12 +103,7 @@ func (normalizer *MinMaxNormalizer) NormalizeValues(updates map[string]message.A
 		if err != nil {
 			return nil, err
 		}
-		for key, update := range updates {
-			normalizedValue := normalizer.getNormalizedValue(update.Values[len(update.Values)-1], upperFence, lowerFence)
-			update.Fields = append(update.Fields, normalizationField)
-			update.Values = append(update.Values, normalizedValue)
-			updates[key] = update
-		}
+		normalizer.normalizeUpdates(updates, upperFence, lowerFence, normalizationField)
 		return statisticalData, nil
 	} else {
 		return nil, fmt.Errorf("No data to normalize for field: %v", inputField)
